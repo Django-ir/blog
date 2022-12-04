9dac046f85d3793109ce006734cc459bfc7003ce
Amirreza Taherkhani <ar.taher404@gmail.com>
2022-12-04 22:01:04 +0330

    create detail view for stories

--- stories/views.py
+++ stories/views.py
@@ -5,15 +5,19 @@ from .models import Story
 
 class StoryListView(generic.ListView):
     model = Story
-    template_name = 'stories/story_list.html'
 
     def get_queryset(self):
         user = self.request.user
-        user_test = get_user_model().objects.get(id=2)
+        user_test = get_user_model().objects.get(id=1)  # temporary test user
+        # followings = user.followings.all()
         queryset = Story.actives.filter(user__in=[user_test])
         return queryset
 
 
+class StoryDetailView(generic.DetailView):
+    model = Story
+
+
 class StoryCreateView(generic.CreateView):
+    model = Story
     fields = ['content', 'caption']
-    template_name = 'stories/story_form.html'
