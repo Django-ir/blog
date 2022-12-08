8ae7105bc80a837ab8d9d646a39de9ca4db00ee2
Amirreza Taherkhani <ar.taher404@gmail.com>
2022-12-08 12:49:23 +0330

    remove story prefix from urls

--- stories/urls_dir/api_urls.py
+++ stories/urls_dir/api_urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from stories import apiviews
 
 urlpatterns = [
-    path('story/', apiviews.StoryListApiView.as_view(), name='api_list_story'),
-    path('story/new/', apiviews.StoryCreateView.as_view(), name='api_create_story'),
-    path('story/<uuid:pk>/', apiviews.StoryDetailView.as_view(), name='api_detail_story'),
+    path('', apiviews.StoryListApiView.as_view(), name='api_list_story'),
+    path('new/', apiviews.StoryCreateView.as_view(), name='api_create_story'),
+    path('<uuid:pk>/', apiviews.StoryDetailView.as_view(), name='api_detail_story'),
 ]
