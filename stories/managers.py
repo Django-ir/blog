28d7a65e43468d02c08db678af961dba75e9365f
Amirreza Taherkhani <ar.taher404@gmail.com>
2022-12-08 12:47:14 +0330

    modify manager so it returns last 24 hours posts

--- stories/managers.py
+++ stories/managers.py
@@ -1,6 +1,12 @@
 from django.db.models import Manager
+from django.utils import timezone
+from datetime import datetime
 
 
 class StoryManager(Manager):
     def get_queryset(self):
-        return super(StoryManager, self).get_queryset().filter(active=True)
+        timestamp_now = datetime.now().timestamp()
+        one_day_in_seconds = 86400
+        time_of_24_hours_ago_in_seconds = timestamp_now - one_day_in_seconds
+        datetime_of_yesterday = datetime.utcfromtimestamp(time_of_24_hours_ago_in_seconds).replace(tzinfo=timezone.utc)
+        return super(StoryManager, self).get_queryset().filter(created__gt=datetime_of_yesterday)
